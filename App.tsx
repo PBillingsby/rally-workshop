2e803ef9e219655cb7c954b13cc839bc1dda6929
PBillingsby <philbillingsby@gmail.com>
2024-01-15 09:51:12 -0500

    add components

--- App.tsx
+++ App.tsx
@@ -1,20 +1,194 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {
+  StyleSheet,
+  Text,
+  View,
+  ScrollView,
+  StatusBar,
+  SafeAreaView,
+  TouchableWithoutFeedback,
+  Keyboard,
+  KeyboardAvoidingView,
+  Platform,
+  TextInput,
+} from "react-native";
+import "@walletconnect/react-native-compat";
+import { WagmiConfig } from "wagmi";
+import { mainnet, polygon, arbitrum } from "viem/chains";
+import {
+  createWeb3Modal,
+  defaultWagmiConfig,
+  Web3Modal,
+} from "@web3modal/wagmi-react-native";
+import Navigation from "./components/Navigation";
+
+import { NavigationContainer } from "@react-navigation/native";
+import { createMaterialTopTabNavigator } from "@react-navigation/material-top-tabs";
+
+import Create from "./components/Create";
+import Verify2 from "./components/Verify";
+import { useKeyboard } from "@react-native-community/hooks";
+import { W3mButton } from "@web3modal/wagmi-react-native";
+import { DataProvider, useDataContext } from "./context/DataContext";
+import Toast from 'react-native-toast-message'
+
+
+// 1. Get projectId at https://cloud.walletconnect.com
+const projectId = "e2183025d1ef9bc2c22b756cd2b250c6";
+
+// 2. Create config
+const metadata = {
+  name: "Web3Modal RN",
+  description: "Web3Modal RN Example",
+  url: "https://web3modal.com",
+  icons: ["https://avatars.githubusercontent.com/u/37784886"],
+  redirect: {
+    native: "YOUR_APP_SCHEME://",
+    universal: "YOUR_APP_UNIVERSAL_LINK.com",
+  },
+};
+
+const chains = [mainnet, polygon, arbitrum];
+
+const wagmiConfig = defaultWagmiConfig({ chains, projectId, metadata });
+
+// 3. Create modal
+createWeb3Modal({
+  projectId,
+  chains,
+  wagmiConfig,
+});
+
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <DataProvider>
+      <WagmiConfig config={wagmiConfig}>
+        <Web3Modal />
+        <View style={styles.container}>
+          <NavigationContainer>
+            <View style={styles.container}>
+              <Navigation />
+            </View>
+          </NavigationContainer>
+          <Toast />
+        </View>
+      </WagmiConfig>
+    </DataProvider>
   );
 }
 
-const styles = StyleSheet.create({
+export const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "rgba(247, 247, 247, 1)",
+    paddingTop: 30,
+  },
+  scrollView: {
+    marginHorizontal: 5,
+  },
+  centerView: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+
+  },
+  body: {
+    backgroundColor: 'white',
+    borderBottomLeftRadius: 0,
+    borderBottomRightRadius: 0,
+    borderTopLeftRadius: 36,
+    borderTopRightRadius: 36,
+    marginTop: 10,
+    height: '100%'
+  },
+  header: {
+    fontSize: 24,
+    fontWeight: "bold",
+    marginBottom: 24,
+    marginTop: 15,
+    flexDirection: 'row', // Align items horizontally
+    alignItems: 'center', // Center items vertically
+  },
+
+  textLabel: {
+    fontSize: 14,
+    fontWeight: "500",
+    color: "#999",
+    marginBottom: 12,
+  },
+  textCode: {
+    backgroundColor: "#eee",
+    fontSize: 16,
+    fontFamily: "monospace",
+    textAlign: "center",
+    padding: 15,
+  },
+  button: {
+    marginBottom: 24,
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+    // backgroundColor: "black",
+    width: "90%",
+    borderRadius: 54,
+    backgroundColor: "rgba(249, 194, 116, 1)",
+  },
+  address: {
+    flexDirection: 'row', // Ensure content is aligned horizontally
+    justifyContent: 'space-between', // Spread items across the available space
+    alignItems: 'center', // Align items vertically
+    backgroundColor: "#F7F7F7",
+    borderRadius: 12,
+    width: '90%',
+    padding: 10,
+    marginBottom: 15
+  },
+  buttonText: {
+    color: "#3E2400",
+    fontWeight: "700",
+    textAlign: "center", // Center text within the button
+    lineHeight: 48, // Adjust as needed
+  },
+  title: {
+    flexDirection: "row",
+    alignItems: "center",
+    marginLeft: 16,
+  },
+  titleText: {
+    marginLeft: 8, // Adjust margin between icon and text
+  },
+  deleteButton: {
+    padding: 10,
+    backgroundColor: "red",
+    borderRadius: 5,
+    // margin: 'auto',
+    height: 50,
+  },
+  deleteText: {
+    color: "white",
+    margin: 0,
+    textAlign: "center",
+    alignContent: "center",
+    alignItems: "center",
+  },
+  text: {
+    fontSize: 16,
+    lineHeight: 21,
+    fontWeight: "bold",
+    letterSpacing: 0.25,
+    color: "white",
+  },
+  textInput2: {
+    width: "90%",
+    // height: 50,
+    borderWidth: 1,
+    borderColor: "#F7F7F7",
+    backgroundColor: "#F7F7F7",
+    paddingHorizontal: 10,
+    marginTop: 10,
+    marginBottom: 10,
+    fontSize: 16,
+    height: 175,
+    borderRadius: 12,
   },
 });
---
+++ components/Navigation.tsx
@@ -0,0 +1,243 @@
+import Create from "./Create";
+import { createMaterialTopTabNavigator } from "@react-navigation/material-top-tabs";
+import {
+  View,
+  Text,
+  StyleSheet,
+  Pressable,
+  TouchableOpacity,
+  Animated,
+  Dimensions,
+} from "react-native";
+import { useWeb3Modal } from "@web3modal/wagmi-react-native";
+import { useAccount, useSignMessage } from "wagmi";
+import React, { useState } from "react";
+import { Entypo, EvilIcons } from "@expo/vector-icons";
+import InputBox from "./InputBox";
+import { styles } from "../App";
+import Verify from "./Verify";
+import { FontAwesome } from "@expo/vector-icons";
+
+// const Tab = createBottomTabNavigator();
+const Tab = createMaterialTopTabNavigator();
+
+function Home() {
+  const { open } = useWeb3Modal();
+  const { address, isConnecting, isDisconnected } = useAccount();
+  const [message, setMessage] = useState("");
+  const { data, isError, isLoading, isSuccess, signMessage } = useSignMessage({
+    message: message,
+  });
+  return (
+    <View
+      style={{
+        flex: 1,
+        backgroundColor: "#fff",
+      }}
+    >
+      <View style={styles.centerView}>
+        <Text style={styles.header}>Create Signature</Text>
+        {!address ? (
+          <>
+            <View
+              style={{
+                borderRadius: 84,
+                overflow: "hidden",
+                margin: 5,
+                width: "90%",
+              }}
+            >
+              <Text style={styles.textCode}>
+                Connect Wallet To Unlock Fields
+              </Text>
+            </View>
+            <Pressable onPress={() => open()} style={styles.button}>
+              <Text style={styles.buttonText}>
+                <Entypo name="wallet" size={14} color="black" />
+                Connect Wallet
+              </Text>
+            </Pressable>
+          </>
+        ) : (
+          <>
+            <Pressable onPress={() => open()} style={styles.button}>
+              <Text
+                style={{
+                  color: "#3E2400",
+                  lineHeight: 48,
+                  fontWeight: "500",
+                }}
+              >
+                addy...
+              </Text>
+            </Pressable>
+          </>
+        )}
+      </View>
+      <View style={styles.title}>
+        <Entypo name="mail" size={19} color="black" />
+        <Text style={styles.titleText}>Message</Text>
+      </View>
+      <View style={styles.centerView}>
+        <InputBox
+          placeholder="Enter message to sign"
+          value={message}
+          onChangeText={(text) => setMessage(text)}
+        />
+        <Pressable
+          disabled={isLoading}
+          onPress={() => signMessage()}
+          style={styles.button}
+        >
+          <Text style={styles.buttonText}>
+            <EvilIcons name="pencil" size={19} color="black" />
+            Generate Signature
+          </Text>
+        </Pressable>
+      </View>
+      <View style={styles.title}>
+        <EvilIcons name="pencil" size={19} color="black" />
+        <Text style={styles.titleText}>Signature</Text>
+      </View>
+      <View style={styles.centerView}>
+        <InputBox
+          placeholder="Enter message to sign"
+          value={data}
+          editable={false}
+        />
+      </View>
+    </View>
+  );
+}
+
+function House2() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        backgroundColor: "#fff",
+      }}
+    >
+      <Create />
+    </View>
+  );
+}
+
+function House() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+        backgroundColor: "red",
+      }}
+    >
+      <Verify />
+    </View>
+  );
+}
+const screenOptions = ({ route }) => ({
+  tabBarShowLabel: true,
+  tabBarLabelStyle: {
+  },
+  headerShown: false,
+  fontSize: 30,
+
+  tabBarStyle: {
+    position: "relative",
+    activeTintColor: "red",
+    padding: 20,
+    borderWidth: 0,
+    borderRadius: 8,
+    margin: 20,
+    color: "red",
+    elevation: 0,
+    height: 100,
+  },
+});
+
+const Navigation = () => {
+  return (
+    <Tab.Navigator
+      tabBar={(props) => <CustomTabBar {...props} />}
+    >
+      <Tab.Screen
+        name="Create"
+        component={Create}
+        options={{ tabBarLabel: '⭐️ Create' }}
+      />
+      <Tab.Screen
+        name="Verify"
+        component={Verify}
+        options={{ tabBarLabel: '✅ Verify' }}
+      />
+    </Tab.Navigator>
+  );
+};
+
+const CustomTabBar = ({ state, descriptors, navigation, position }) => {
+  const inputRange = state.routes.map((_, index) => index);
+  const { width } = Dimensions.get('window');
+
+  const translateX = Animated.multiply(position, width / state.routes.length);
+
+  return (
+    <View style={{ flexDirection: 'row', backgroundColor: 'lightgrey', borderRadius: 42, margin: 10 }}>
+      {state.routes.map((route, index) => {
+        const isFocused = state.index === index;
+        const tabBarStyle = {
+          borderRadius: 12,
+          zIndex: 1,
+          flex: 1,
+          alignItems: 'center',
+          justifyContent: 'center',
+          marginHorizontal: 8,
+          height: 50,
+          // backgroundColor: 'red',
+        };
+
+        return (
+          <TouchableOpacity
+            key={index}
+            onPress={() => navigation.navigate(route.name)}
+            style={tabBarStyle}
+          >
+            <Text style={{ color: isFocused ? 'black' : 'black' }}>
+              {descriptors[route.key].options.tabBarLabel}
+            </Text>
+          </TouchableOpacity>
+        );
+      })}
+      <Animated.View
+        style={[
+          istyles.tabIndicator,
+          {
+            width: `${100 / state.routes.length}%`,
+            transform: [{ translateX }],
+          },
+        ]}
+      />
+    </View>
+  );
+};
+const istyles = StyleSheet.create({
+  tabBarButton: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderRadius: 12,
+    marginHorizontal: 8,
+    padding: 20,
+    backgroundColor: 'red'
+  },
+  tabIndicator: {
+    position: 'absolute',
+    bottom: 0,
+    height: '100%',
+    backgroundColor: 'white',
+    borderRadius: 42,
+  },
+});
+
+export default Navigation;
